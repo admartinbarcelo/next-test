4e8d499213ecc5520b0a41e17de4e267aaccc440
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2024-05-09 11:12:26 +0200

    chore: Update package.json dev script to use Next.js turbo mode
    Add the `--turbo` flag to the `dev` script in `package.json` to enable Next.js turbo mode, which improves the development server's performance.
    
    Create new pages for Works, About, and Contact
    
    Add new pages for Works, About, and Contact in the `app/(public)` directory. Each page includes metadata for SEO purposes and a simple placeholder content.
    
    Update metadata in layout.tsx
    
    Update the `title` and `description` in the `metadata` object in `layout.tsx` to reflect the specific application name and description.
    
    Create GeneralLayout component
    
    Create the `GeneralLayout` component in `app/(public)/layout.tsx` to provide a common layout structure for all pages. The component includes a main container with a greeting message and renders the children components.

---
+++ app/(public)/about/page.tsx
@@ -0,0 +1,9 @@
+
+export const metadata = {
+ title: 'SEO Title for About Page',
+ description: ['About Page', 'Adrian Martin', 'Description', 'Frontend Developer'],
+};
+
+export default function AboutPage() {
+  return <span className="text-7xl">AboutPage</span>;
+}
---
+++ app/(public)/works/page.tsx
@@ -0,0 +1,9 @@
+
+export const metadata = {
+ title: 'SEO Title for Work Page',
+ description: ['Work Page', 'Adrian Martin', 'Description', 'Frontend Developer'],
+};
+
+export default function WorkPage() {
+  return <span className="text-7xl">WorkPage</span>;
+}
